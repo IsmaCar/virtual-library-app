631491e92f824ebc82bb8f446f300a13ed1c43b9
IsmaCar <carballomartinismael@gmail.com>
2025-11-03 15:23:51 +0100

    Crear paginación

--- frontend/src/components/BookSearch.jsx
+++ frontend/src/components/BookSearch.jsx
@@ -5,6 +5,15 @@ const API_BOOKS_URL = import.meta.env.VITE_OPENLIBRARY_URL
 function BookSearch() {
     const [formData, setFormData] = useState({ query: "" })
     const [books, setBooks] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const booksPerPage = 9
+    const startIndex = (currentPage - 1) * booksPerPage
+    const endIndex = startIndex + booksPerPage
+
+    const booksToShow = books.slice(startIndex, endIndex)
+
+    const totalPages = Math.ceil(books.length / booksPerPage)
 
     const handleChange = (e) => {
         setFormData(prev => ({ ...prev, [e.target.name]: e.target.value }))
@@ -34,12 +43,17 @@ function BookSearch() {
                         onChange={handleChange}
                         value={formData.query}
                         required
-                        className="border border-gray-300 rounded-lg px-4 py-2 w-64 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 shadow-sm"
+                        className="border border-gray-300 rounded-lg px-4 py-2 w-64 
+                                   focus:outline-none focus:ring-2 focus:ring-blue-500 
+                                 focus:border-blue-500 shadow-sm"
                         placeholder="Buscar libro o autor..."
                     />
                     <button
                         type='submit'
-                        className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-200 shadow-sm"
+                        className="bg-blue-600 text-white px-6 py-2 rounded-lg 
+                                   hover:bg-blue-700 focus:outline-none 
+                                   focus:ring-2 focus:ring-blue-500 
+                                   focus:ring-offset-2 transition duration-200 shadow-sm"
                     >
                         Buscar
                     </button>
@@ -47,26 +61,41 @@ function BookSearch() {
             </section>
             {/*Resultado (tarjetas), del buscador de libros*/}
             <section>
-                {books.length > 0 ? (
-                    <article>
-                        {books.map(book => (
-                            <div key={book.key}
-                                className="bg-white rounded-lg shadow p-4 flex flex-col items-center">
-                                <img
-                                    src={getCoverUrl(book.cover_i)}
-                                    alt={book.title}
-                                    className="mb-2 w-32 h-48 object-cover rounded"
-                                />
-                                <h2 className='text-lg font-bold mb-2'>{book.title}</h2>
-                                <p className='text-gray-700 mb-1'>
-                                    Autor: {formatAuthors(book.author_name)}
-                                </p>
-                                <p className='text-gray-500 mb-1'>
-                                    Año: {formatYear(book.first_publish_year)}
-                                </p>
-                            </div>
-                        ))}
-                    </article>
+                {booksToShow.length > 0 ? (
+                    <>
+                        <article className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
+                            {booksToShow.map(book => (
+                                <div key={book.key}
+                                    className="bg-white shadow p-4 flex flex-col items-center mb-5 ml-5 mr-5">
+                                    <img
+                                        src={getCoverUrl(book.cover_i)}
+                                        alt={book.title}
+                                        className="mb-2 w-32 h-48 object-cover rounded"
+                                    />
+                                    <h2 className='text-lg font-bold mb-2'>{book.title}</h2>
+                                    <p className='text-gray-700 mb-1'>
+                                        Autor: {formatAuthors(book.author_name)}
+                                    </p>
+                                    <p className='text-gray-500 mb-1'>
+                                        Año: {formatYear(book.first_publish_year)}
+                                    </p>
+
+                                </div>
+                            ))}
+                        </article>
+                        {/*Botones de paginación*/}
+                        <div className="flex justify-center mt-4 gap-2">
+                            {Array.from({ length: totalPages }, (_, i) => (
+                                <button
+                                    key={i}
+                                    onClick={() => setCurrentPage(i + 1)}
+                                    className={`px-3 py-1 rounded ${currentPage === i + 1 ? 'bg-blue-600 text-white' : 'bg-gray-200'}`}
+                                >
+                                    {i + 1}
+                                </button>
+                            ))}
+                        </div>
+                    </>
                 ) : null}
             </section>
         </>
