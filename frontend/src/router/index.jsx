552ce4ee452c4caad7fa175f06e3af6204d23112
IsmaCar <carballomartinismael@gmail.com>
2025-11-09 15:30:39 +0100

    Crear ruta a vista detallada

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 import Home from "../pages/Home"
 import RootLayout from "../layouts/RootLayout"
+import BookDetailPage from "../pages/BookDetailPage"
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
         {
             index: true,
             element: <Home/>
+        },
+        {
+            path: "book/:id",
+            element: <BookDetailPage/>
         }]
     }
 ])
